1f1162a1e98f9bcc368f65d870f3693f9a200d56
Grant Forrest <gaforres@gmail.com>
2020-07-03 15:22:00 -0400

    build and push artifacts

---
+++ ui/build/precache-manifest.78042793d9ea4ed58f50ffb3b6a1843a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4553b4206ccc12b403885602787d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03622419c4077e5489b7",
+    "url": "/static/css/main.90502136.chunk.css"
+  },
+  {
+    "revision": "5d0fed3872f8d91c53e8",
+    "url": "/static/js/2.a1cfe7b4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a1cfe7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03622419c4077e5489b7",
+    "url": "/static/js/main.8bd51837.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
